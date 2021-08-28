58cc39b75482c05cbb48e291ebccfac963fae47a
mi-wada <mtak.wd@gmail.com>
2021-08-28 17:56:19 +0900

    fix env for mysql

--- api/app/lib/db.go
+++ api/app/lib/db.go
@@ -10,15 +10,15 @@ import (
 )
 
 var (
-	Dialect    = "mysql"
-	DBUser     = os.Getenv("MYSQL_USER")
-	DBPass     = os.Getenv("MYSQL_PASSWORD")
-	DBProtocol = "tcp(db:3306)"
+	DBUsername = os.Getenv("MYSQL_USERNAME")
+	DBPassword = os.Getenv("MYSQL_PASSWORD")
+	DBHost     = os.Getenv("MYSQL_HOST")
+	DBPort     = os.Getenv("MYSQL_PORT")
 	DBName     = os.Getenv("MYSQL_DATABASE")
 )
 
 func ConnectDB() *gorm.DB {
-	dsn := fmt.Sprintf("%s:%s@%s/%s?parseTime=true", DBUser, DBPass, DBProtocol, DBName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", DBUsername, DBPassword, DBHost, DBPort, DBName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err.Error())
