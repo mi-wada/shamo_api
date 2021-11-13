4a2475331c4e76ca63e06db613632463152e28f6
mi-wada <mtak.wd@gmail.com>
2021-11-13 23:48:36 +0900

    fix payment

--- api/app/model/payment.go
+++ api/app/model/payment.go
@@ -3,8 +3,8 @@ package model
 type Payment struct {
 	Id       string `dynamo:"id"`
 	Price    int    `json:"price" dynamo:"price"`
-	Room_id  string `json:"room_id" param:"roomId" dynamo:"room_id"`
-	User_id  string `json:"user_id" dynamo:"user_id"`
+	RoomId   string `json:"room_id" param:"roomId" dynamo:"room_id"`
+	UserId   int    `json:"user_id" dynamo:"user_id"`
 	What     string `json:"what" dynamo:"what"`
 	Is_valid bool
 }
