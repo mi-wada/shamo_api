106c6dd25e3cd9c0fbfde9ac4161ca09e4da1865
mi-wada <mtak.wd@gmail.com>
2021-11-13 22:25:49 +0900

    use dynamo

--- api/app/service/user.go
+++ api/app/service/user.go
@@ -1,27 +1,35 @@
 package service
 
 import (
-	"api/app/lib"
 	"api/app/model"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/guregu/dynamo"
 )
 
 func CreateUser(user *model.User) *model.User {
-	db := lib.ConnectDB()
-	closer, _ := db.DB()
-	defer closer.Close()
+	sess := session.Must(session.NewSession())
+	db := dynamo.New(sess, &aws.Config{Region: aws.String(os.Getenv("AWS_DEFAULT_REGION"))})
+	table := db.Table("shamo_user")
 
-	db.Create(user)
+	err := table.Put(user).Run()
+
+	if err != nil {
+		println(err.Error())
+	}
 
 	return user
 }
 
 func GetUsersByRoomId(roomId string) []model.User {
-	db := lib.ConnectDB()
-	closer, _ := db.DB()
-	defer closer.Close()
+	sess := session.Must(session.NewSession())
+	db := dynamo.New(sess, &aws.Config{Region: aws.String(os.Getenv("AWS_DEFAULT_REGION"))})
+	table := db.Table("shamo_user")
 
 	var users []model.User
-	db.Find(&users, "Room_id = ?", roomId)
+	table.Scan().Filter("room_id = ?", roomId).All(users)
 
 	return users
 }
