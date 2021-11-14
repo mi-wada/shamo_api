a66c1cfc00dc019894e4f1cc6cb2c3fc7ab9a7e7
mi-wada <mtak.wd@gmail.com>
2021-11-14 20:54:25 +0900

    fix config for server

--- api/config/server.go
+++ api/config/server.go
@@ -26,7 +26,5 @@ func SettingForServer(server *echo.Echo) {
 		fmt.Fprintf(os.Stderr, "Request: %v\n", string(reqBody))
 		fmt.Fprintf(os.Stderr, "Headers: %v\n", c.Request().Header)
 		fmt.Fprintf(os.Stderr, "Method: %v\n", c.Request().Method)
-		fmt.Fprintf(os.Stderr, "MY_AWS_ACCESS_KEY_ID: %v\n", os.Getenv("MY_AWS_ACCESS_KEY_ID"))
-		fmt.Fprintf(os.Stderr, "AWS_ACCESS_KEY_ID: %v\n", os.Getenv("AWS_ACCESS_KEY_ID"))
 	}))
 }
