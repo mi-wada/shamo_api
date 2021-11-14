d31c93fd524366ac3d294ccbc2bdfa596f148bf9
mi-wada <mtak.wd@gmail.com>
2021-11-14 12:14:50 +0900

    fix payment

--- api/api/index.go
+++ api/api/index.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"api/config"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
@@ -12,6 +13,10 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	config.SettingForServer(server)
 	config.SetRoutes(server)
+
+	os.Setenv("AWS_ACCESS_KEY_ID", os.Getenv("MY_AWS_ACCESS_KEY_ID"))
+	os.Setenv("AWS_SECRET_ACCESS_KEY", os.Getenv("MY_AWS_SECRET_ACCESS_KEY"))
+	os.Setenv("AWS_DEFAULT_REGION", os.Getenv("MY_AWS_DEFAULT_REGION"))
 
 	server.ServeHTTP(w, r)
 }
