88cd0e9effa6d13aeefec95421513038e0039579
mi-wada <mtak.wd@gmail.com>
2021-11-14 11:53:03 +0900

    add setting for vercel

--- api/api/index.go
+++ api/api/index.go
@@ -1,16 +1,18 @@
-package main
+package handler
 
 import (
 	"api/config"
+	"fmt"
+	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
 
-func main() {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	server := echo.New()
 
 	config.SettingForServer(server)
 	config.SetRoutes(server)
 
-	server.Logger.Fatal(server.Start(":8080"))
+	fmt.Fprintf(w, r.URL.Path)
 }
