33c919ffe2201da86aef5df74a52d5baeac3d8d0
mi-wada <mtak.wd@gmail.com>
2021-11-15 14:54:35 +0900

    remove unnecessary files

--- api/app/model/room.go
+++
@@ -1,6 +0,0 @@
-package model
-
-type Room struct {
-	Id      int    `dynamo:"id"`
-	Room_id string `dynamo:"room_id"`
-}
--- api/app/model/user.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type User struct {
-	Id      int    `dynamo:"id"`
-	Name    string `dynamo:"name"`
-	Room_id string `dynamo:"room_id"`
-}
